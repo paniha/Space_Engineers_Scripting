f038fa8dda6939542c2d535dccc32d946e848af1
paniha <38439554+paniha@users.noreply.github.com>
2019-10-30 22:31:23 +0100

    Function for turning World coordinates into local

---
+++ LocalCoords.cs
@@ -0,0 +1,24 @@
+        /*
+		Functions for turning global coordinates into local ones, relative to a cockpit or remote control.
+		
+		Usage:
+		
+		Paste the two functions (or the one you need, they both do the same) into the bottom of your script, outside the main loop
+		
+		Vector3D LocalPos = LocalCoords(WorldPos,cockpit);
+		
+		or
+		
+		Vector3D LocalPos = LocalCoords(WorldPos,remote);
+	
+		*/
+		
+		public Vector3D LocalCoords(Vector3D worldPos,IMyCockpit cockpit)
+        {
+            return RoundVector(Vector3D.TransformNormal(worldPos - cockpit.GetPosition(), MatrixD.Transpose(cockpit.WorldMatrix)));
+        }
+
+        public Vector3D LocalCoords(Vector3D worldPos, IMyRemoteControl cockpit)
+        {
+            return RoundVector(Vector3D.TransformNormal(worldPos - cockpit.GetPosition(), MatrixD.Transpose(cockpit.WorldMatrix)));
+        }
