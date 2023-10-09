387b0c66a5ec7dd32207cfb46fd9732e9e2d8865
paniha <38439554+paniha@users.noreply.github.com>
2023-10-09 21:55:47 +0200

    Create LightScript.cs

---
+++ LightScript.cs
@@ -0,0 +1,631 @@
+        Color GetColor(string colorName)
+        {
+            Color Col;
+            switch (colorName.ToLower())
+            {
+                case "aliceblue":
+                    Col = Color.AliceBlue;
+                    break;
+
+                case "antiquewhite":
+                    Col = Color.AntiqueWhite;
+                    break;
+
+                case "aqua":
+                    Col = Color.Aqua;
+                    break;
+
+                case "aquamarine":
+                    Col = Color.Aquamarine;
+                    break;
+
+                case "azure":
+                    Col = Color.Azure;
+                    break;
+
+                case "beige":
+                    Col = Color.Beige;
+                    break;
+
+                case "bisque":
+                    Col = Color.Bisque;
+                    break;
+
+                case "black":
+                    Col = Color.Black;
+                    break;
+
+                case "blanchedalmond":
+                    Col = Color.BlanchedAlmond;
+                    break;
+
+                case "blue":
+                    Col = Color.Blue;
+                    break;
+
+                case "blueviolet":
+                    Col = Color.BlueViolet;
+                    break;
+
+                case "brown":
+                    Col = Color.Brown;
+                    break;
+
+                case "burlywood":
+                    Col = Color.BurlyWood;
+                    break;
+
+                case "cadetblue":
+                    Col = Color.CadetBlue;
+                    break;
+
+                case "chartreuse":
+                    Col = Color.Chartreuse;
+                    break;
+
+                case "chocolate":
+                    Col = Color.Chocolate;
+                    break;
+
+                case "coral":
+                    Col = Color.Coral;
+                    break;
+
+                case "cornflowerblue":
+                    Col = Color.CornflowerBlue;
+                    break;
+
+                case "cornsilk":
+                    Col = Color.Cornsilk;
+                    break;
+
+                case "crimson":
+                    Col = Color.Crimson;
+                    break;
+
+                case "cyan":
+                    Col = Color.Cyan;
+                    break;
+
+                case "darkblue":
+                    Col = Color.DarkBlue;
+                    break;
+
+                case "darkcyan":
+                    Col = Color.DarkCyan;
+                    break;
+
+                case "darkgoldenrod":
+                    Col = Color.DarkGoldenrod;
+                    break;
+
+                case "darkgray":
+                    Col = Color.DarkGray;
+                    break;
+
+                case "darkgreen":
+                    Col = Color.DarkGreen;
+                    break;
+
+                case "darkkhaki":
+                    Col = Color.DarkKhaki;
+                    break;
+
+                case "darkmagenta":
+                    Col = Color.DarkMagenta;
+                    break;
+
+                case "darkolivegreen":
+                    Col = Color.DarkOliveGreen;
+                    break;
+
+                case "darkorange":
+                    Col = Color.DarkOrange;
+                    break;
+
+                case "darkorchid":
+                    Col = Color.DarkOrchid;
+                    break;
+
+                case "darkred":
+                    Col = Color.DarkRed;
+                    break;
+
+                case "darksalmon":
+                    Col = Color.DarkSalmon;
+                    break;
+
+                case "darkseagreen":
+                    Col = Color.DarkSeaGreen;
+                    break;
+
+                case "darkslateblue":
+                    Col = Color.DarkSlateBlue;
+                    break;
+
+                case "darkslategray":
+                    Col = Color.DarkSlateGray;
+                    break;
+
+                case "darkturquoise":
+                    Col = Color.DarkTurquoise;
+                    break;
+
+                case "darkviolet":
+                    Col = Color.DarkViolet;
+                    break;
+
+                case "deeppink":
+                    Col = Color.DeepPink;
+                    break;
+
+                case "deepskyblue":
+                    Col = Color.DeepSkyBlue;
+                    break;
+
+                case "dimgray":
+                    Col = Color.DimGray;
+                    break;
+
+                case "dodgerblue":
+                    Col = Color.DodgerBlue;
+                    break;
+
+                case "firebrick":
+                    Col = Color.Firebrick;
+                    break;
+
+                case "floralwhite":
+                    Col = Color.FloralWhite;
+                    break;
+
+                case "forestgreen":
+                    Col = Color.ForestGreen;
+                    break;
+
+                case "fuchsia":
+                    Col = Color.Fuchsia;
+                    break;
+
+                case "gainsboro":
+                    Col = Color.Gainsboro;
+                    break;
+
+                case "ghostwhite":
+                    Col = Color.GhostWhite;
+                    break;
+
+                case "gold":
+                    Col = Color.Gold;
+                    break;
+
+                case "goldenrod":
+                    Col = Color.Goldenrod;
+                    break;
+
+                case "gray":
+                    Col = Color.Gray;
+                    break;
+
+                case "green":
+                    Col = Color.Green;
+                    break;
+
+                case "greenyellow":
+                    Col = Color.GreenYellow;
+                    break;
+
+                case "honeydew":
+                    Col = Color.Honeydew;
+                    break;
+
+                case "hotpink":
+                    Col = Color.HotPink;
+                    break;
+
+                case "indianred":
+                    Col = Color.IndianRed;
+                    break;
+
+                case "indigo":
+                    Col = Color.Indigo;
+                    break;
+
+                case "ivory":
+                    Col = Color.Ivory;
+                    break;
+
+                case "khaki":
+                    Col = Color.Khaki;
+                    break;
+
+                case "lavender":
+                    Col = Color.Lavender;
+                    break;
+
+                case "lavenderblush":
+                    Col = Color.LavenderBlush;
+                    break;
+
+                case "lawngreen":
+                    Col = Color.LawnGreen;
+                    break;
+
+                case "lemonchiffon":
+                    Col = Color.LemonChiffon;
+                    break;
+
+                case "lightblue":
+                    Col = Color.LightBlue;
+                    break;
+
+                case "lightcoral":
+                    Col = Color.LightCoral;
+                    break;
+
+                case "lightcyan":
+                    Col = Color.LightCyan;
+                    break;
+
+                case "lightgoldenrodyellow":
+                    Col = Color.LightGoldenrodYellow;
+                    break;
+
+                case "lightgray":
+                    Col = Color.LightGray;
+                    break;
+
+                case "lightgreen":
+                    Col = Color.LightGreen;
+                    break;
+
+                case "lightpink":
+                    Col = Color.LightPink;
+                    break;
+
+                case "lightsalmon":
+                    Col = Color.LightSalmon;
+                    break;
+
+                case "lightseagreen":
+                    Col = Color.LightSeaGreen;
+                    break;
+
+                case "lightskyblue":
+                    Col = Color.LightSkyBlue;
+                    break;
+
+                case "lightslategray":
+                    Col = Color.LightSlateGray;
+                    break;
+
+                case "lightsteelblue":
+                    Col = Color.LightSteelBlue;
+                    break;
+
+                case "lightyellow":
+                    Col = Color.LightYellow;
+                    break;
+
+                case "lime":
+                    Col = Color.Lime;
+                    break;
+
+                case "limegreen":
+                    Col = Color.LimeGreen;
+                    break;
+
+                case "linen":
+                    Col = Color.Linen;
+                    break;
+
+                case "magenta":
+                    Col = Color.Magenta;
+                    break;
+
+                case "maroon":
+                    Col = Color.Maroon;
+                    break;
+
+                case "mediumaquamarine":
+                    Col = Color.MediumAquamarine;
+                    break;
+
+                case "mediumblue":
+                    Col = Color.MediumBlue;
+                    break;
+
+                case "mediumorchid":
+                    Col = Color.MediumOrchid;
+                    break;
+
+                case "mediumpurple":
+                    Col = Color.MediumPurple;
+                    break;
+
+                case "mediumseagreen":
+                    Col = Color.MediumSeaGreen;
+                    break;
+
+                case "mediumslateblue":
+                    Col = Color.MediumSlateBlue;
+                    break;
+
+                case "mediumspringgreen":
+                    Col = Color.MediumSpringGreen;
+                    break;
+
+                case "mediumturquoise":
+                    Col = Color.MediumTurquoise;
+                    break;
+
+                case "mediumvioletred":
+                    Col = Color.MediumVioletRed;
+                    break;
+
+                case "midnightblue":
+                    Col = Color.MidnightBlue;
+                    break;
+
+                case "mintcream":
+                    Col = Color.MintCream;
+                    break;
+
+                case "mistyrose":
+                    Col = Color.MistyRose;
+                    break;
+
+                case "moccasin":
+                    Col = Color.Moccasin;
+                    break;
+
+                case "navajowhite":
+                    Col = Color.NavajoWhite;
+                    break;
+
+                case "navy":
+                    Col = Color.Navy;
+                    break;
+
+                case "oldlace":
+                    Col = Color.OldLace;
+                    break;
+
+                case "olive":
+                    Col = Color.Olive;
+                    break;
+
+                case "olivedrab":
+                    Col = Color.OliveDrab;
+                    break;
+
+                case "orange":
+                    Col = Color.Orange;
+                    break;
+
+                case "orangered":
+                    Col = Color.OrangeRed;
+                    break;
+
+                case "orchid":
+                    Col = Color.Orchid;
+                    break;
+
+                case "palegoldenrod":
+                    Col = Color.PaleGoldenrod;
+                    break;
+
+                case "palegreen":
+                    Col = Color.PaleGreen;
+                    break;
+
+                case "paleturquoise":
+                    Col = Color.PaleTurquoise;
+                    break;
+
+                case "palevioletred":
+                    Col = Color.PaleVioletRed;
+                    break;
+
+                case "papayawhip":
+                    Col = Color.PapayaWhip;
+                    break;
+
+                case "peachpuff":
+                    Col = Color.PeachPuff;
+                    break;
+
+                case "peru":
+                    Col = Color.Peru;
+                    break;
+
+                case "pink":
+                    Col = Color.Pink;
+                    break;
+
+                case "plum":
+                    Col = Color.Plum;
+                    break;
+
+                case "powderblue":
+                    Col = Color.PowderBlue;
+                    break;
+
+                case "purple":
+                    Col = Color.Purple;
+                    break;
+
+                case "red":
+                    Col = Color.Red;
+                    break;
+
+                case "rosybrown":
+                    Col = Color.RosyBrown;
+                    break;
+
+                case "royalblue":
+                    Col = Color.RoyalBlue;
+                    break;
+
+                case "saddlebrown":
+                    Col = Color.SaddleBrown;
+                    break;
+
+                case "salmon":
+                    Col = Color.Salmon;
+                    break;
+
+                case "sandybrown":
+                    Col = Color.SandyBrown;
+                    break;
+
+                case "seagreen":
+                    Col = Color.SeaGreen;
+                    break;
+
+                case "seashell":
+                    Col = Color.SeaShell;
+                    break;
+
+                case "sienna":
+                    Col = Color.Sienna;
+                    break;
+
+                case "silver":
+                    Col = Color.Silver;
+                    break;
+
+                case "skyblue":
+                    Col = Color.SkyBlue;
+                    break;
+
+                case "slateblue":
+                    Col = Color.SlateBlue;
+                    break;
+
+                case "slategray":
+                    Col = Color.SlateGray;
+                    break;
+
+                case "snow":
+                    Col = Color.Snow;
+                    break;
+
+                case "springgreen":
+                    Col = Color.SpringGreen;
+                    break;
+
+                case "steelblue":
+                    Col = Color.SteelBlue;
+                    break;
+
+                case "tan":
+                    Col = Color.Tan;
+                    break;
+
+                case "teal":
+                    Col = Color.Teal;
+                    break;
+
+                case "thistle":
+                    Col = Color.Thistle;
+                    break;
+
+                case "tomato":
+                    Col = Color.Tomato;
+                    break;
+
+                case "transparent":
+                    Col = Color.Transparent;
+                    break;
+
+                case "turquoise":
+                    Col = Color.Turquoise;
+                    break;
+
+                case "violet":
+                    Col = Color.Violet;
+                    break;
+
+                case "wheat":
+                    Col = Color.Wheat;
+                    break;
+
+                case "white":
+                    Col = Color.White;
+                    break;
+
+                case "whitesmoke":
+                    Col = Color.WhiteSmoke;
+                    break;
+
+                case "yellow":
+                    Col = Color.Yellow;
+                    break;
+
+                default:
+                    throw new InvalidOperationException("Invalid color");
+                    break;
+            }
+            return Col;
+        }
+        public Program()
+        {
+            // The constructor, called only once every session and
+            // always before any other method is called. Use it to
+            // initialize your script. 
+            //     
+            // The constructor is optional and can be removed if not
+            // needed.
+            // 
+            // It's recommended to set Runtime.UpdateFrequency 
+            // here, which will allow your script to run itself without a 
+            // timer block.
+        }
+
+        bool grouped = false;
+        public void Main(string argument, UpdateType updateSource)
+        {
+            string[] args = argument.Split(' ');
+            IMyLightingBlock light = GridTerminalSystem.GetBlockWithName(args[0]) as IMyLightingBlock;
+            IMyBlockGroup lightgroup;
+            List<IMyTerminalBlock> lights = new List<IMyTerminalBlock>();
+            if (light == null)
+            {
+                lightgroup = GridTerminalSystem.GetBlockGroupWithName(args[0]);
+                if (lightgroup == null) { 
+                    throw new InvalidOperationException("Invalid name");
+                } else {
+                    grouped = true;
+                    lightgroup.GetBlocks(lights);
+                }
+            }
+            if (grouped) { 
+                for (int i = 0; i < lights.Count; i++) { 
+                    if(args.Length > 2)
+                    {
+                        if(((IMyLightingBlock)lights[i]).Color == GetColor(args[1]))
+                            ((IMyLightingBlock)lights[i]).Color = GetColor(args[2]);
+                        else
+                            ((IMyLightingBlock)lights[i]).Color = GetColor(args[1]);
+                    }
+                    else
+                    {
+                        ((IMyLightingBlock)lights[i]).Color = GetColor(args[1]);
+                    }
+                }
+            }
+            else { 
+                if(args.Length > 2)
+                {
+                    if(light.Color == GetColor(args[1]))
+                        light.Color = GetColor(args[2]);
+                    else
+                        light.Color = GetColor(args[1]);
+                }
+                else
+                {
+                    light.Color = GetColor(args[1]);
+                }
+            }
+        }
